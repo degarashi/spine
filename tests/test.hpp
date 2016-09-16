b853ae2d34ecd740e77d4f04445aae332e2a3d3a
degarashi <slice013@gmail.com>
2016-09-16 11:14:34 +0900

    freaからシリアライズのチェックコードを移植

--- tests/test.hpp
+++ tests/test.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "../random.hpp"
 #include <gtest/gtest.h>
+#include <cereal/archives/json.hpp>
+#include <cereal/archives/binary.hpp>
 
 namespace spi {
 	namespace test {
@@ -14,5 +16,37 @@ namespace spi {
 				}
 		};
 		#define USING(t) using t = typename TestFixture::t
+
+		// from frea:(tests/test.hpp) 84623829a7e9e814d0632e96bc560dc62c3a4cfd
+		// ---- for serialization check ----
+		template <class OA, class IA, class T>
+		void _CheckSerialization(const T& src) {
+			std::stringstream buffer;
+			{
+				OA oa(buffer);
+				oa(CEREAL_NVP(src));
+			}
+			T loaded;
+			{
+				IA ia(buffer);
+				ia(cereal::make_nvp("src", loaded));
+			}
+			ASSERT_EQ(src, loaded);
+		}
+		template <class T>
+		void CheckSerializationBin(const T& src) {
+			_CheckSerialization<cereal::BinaryOutputArchive,
+								cereal::BinaryInputArchive>(src);
+		}
+		template <class T>
+		void CheckSerializationJSON(const T& src) {
+			_CheckSerialization<cereal::JSONOutputArchive,
+								cereal::JSONInputArchive>(src);
+		}
+		template <class T>
+		void CheckSerialization(const T& src) {
+			CheckSerializationBin(src);
+			CheckSerializationJSON(src);
+		}
 	}
 }
