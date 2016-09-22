d7ae411aec503470d3d1eaa83d71f57e0a800888
degarashi <slice013@gmail.com>
2016-09-22 14:51:20 +0900

    名前付きリソースマネージャ追加

---
+++ resmgr_named.hpp
@@ -0,0 +1,79 @@
+#pragma once
+#include "singleton.hpp"
+#include "restag.hpp"
+#include "optional.hpp"
+#include <cereal/types/unordered_map.hpp>
+
+namespace spi {
+	//! 名前付きリソースマネージャ
+	/*!
+		中身の保持はすべてスマートポインタで行う
+		シングルスレッド動作
+	*/
+	template <class T, class K=std::string>
+	class ResMgrName : public Singleton<ResMgrName<T,K>> {
+		public:
+			using value_t = T;
+			using shared_t = std::shared_ptr<value_t>;
+			using key_t = K;
+			template <class M>
+			friend struct ResDeleter;
+		private:
+			using this_t = ResMgrName<T,K>;
+			using tag_t = ResTag<value_t>;
+			using Resource = std::unordered_map<key_t, tag_t>;
+			using Val2Key = std::unordered_map<const value_t*, key_t>;
+			Resource	_resource;
+			Val2Key		_v2k;
+
+			void _release(value_t* p) noexcept {
+				const auto itr = _v2k.find(p);
+				try {
+					Assert0(itr != _v2k.end());
+					const auto itr2 = _resource.find(itr->second);
+					Assert0(itr2 != _resource.end());
+					_v2k.erase(itr);
+					_resource.erase(itr2);
+					p->~value_t();
+				} catch(...) {}
+			}
+
+		public:
+			template <class Ar>
+			void serialize(Ar& ar) {
+				ar(_resource);
+			}
+			template <class... Ts>
+			auto acquire(const key_t& k, Ts&&... ts) {
+				// 既に同じ名前でリソースを確保済みならばそれを返す
+				if(auto ret = get(k))
+					return ret;
+				// 新しくリソースを作成
+				shared_t p(new value_t(std::forward<Ts>(ts)...), ResDeleter<this_t>());
+				_resource.emplace(k, p);
+				_v2k.emplace(p.get(), k);
+				return p;
+			}
+			Optional<const key_t&> getKey(const shared_t& p) const {
+				return getKey(p.get());
+			}
+			Optional<const key_t&> getKey(const value_t* p) const {
+				const auto itr = _v2k.find(p);
+				if(itr != _v2k.end())
+					return itr->second;
+				return none;
+			}
+			shared_t get(const key_t& k) {
+				const auto itr = _resource.find(k);
+				if(itr != _resource.end()) {
+					shared_t ret(itr->second.weak.lock());
+					D_Assert0(ret);
+					return ret;
+				}
+				return nullptr;
+			}
+			std::size_t size() const noexcept {
+				return _resource.size();
+			}
+	};
+}
--- tests/resource_test.cpp
+++ tests/resource_test.cpp
@@ -1,5 +1,7 @@
 #include "test.hpp"
 #include "../resmgr.hpp"
+#include "../resmgr_named.hpp"
+#include "../lubee/random/string.hpp"
 
 namespace spi {
 	namespace test {
@@ -54,6 +56,115 @@ namespace spi {
 			// 各リソースの内部値が合致しているか
 			for(auto& r : res) {
 				ASSERT_EQ(r.second, r.first->getValue());
+			}
+			// コンストラクタ/デストラクタが呼ばれた回数を比較
+			// (ctor - dtor == リソースの残り数)
+			ASSERT_NO_FATAL_FAILURE(CheckCounter<TypeParam>(res.size()));
+			// こちらで管理しているリソースハンドルとリソースマネージャのそれは一致する
+			ASSERT_EQ(res.size(), mgr.size());
+			res.clear();
+			// (ctor - dtor == 0)
+			ASSERT_NO_FATAL_FAILURE(CheckCounter<TypeParam>(0));
+			// 全てリソースを開放したのでマネージャのリソース数もゼロになる
+			ASSERT_EQ(0, mgr.size());
+		}
+
+		template <class T>
+		struct ResourceMgrName : Random {
+			using rmgr_t = ResMgrName<T>;
+			rmgr_t	_mgr;
+			using value_t = typename T::value_t;
+			using res_t = typename rmgr_t::shared_t;
+			using key_t = typename rmgr_t::key_t;
+
+			auto& getMgr() {
+				return _mgr;
+			}
+		};
+		using NTypes = ::testing::Types<TestObj<int>>;
+		TYPED_TEST_CASE(ResourceMgrName, NTypes);
+
+		struct ActionN {
+			enum e {
+				Acquire,
+				Release,
+				AcquireExist,
+				FindByName,
+				FindByHandle,
+				_Num
+			};
+		};
+		TYPED_TEST(ResourceMgrName, General) {
+			USING(key_t);
+			USING(res_t);
+			USING(value_t);
+			InitializeCounter<TypeParam>();
+
+			struct Res {
+				key_t		key;
+				res_t		res;
+				value_t		val;
+			};
+			using RMap = std::unordered_map<key_t, Res>;
+			const auto mtf = this->mt().template getUniformF<int>();
+			const auto rv = this->mt().template getUniformF<value_t>();
+			auto& mgr = this->getMgr();
+
+			RMap res;
+			const auto pickRandom = [&res, &mtf](){
+				const int idx = mtf({0, int(res.size()-1)});
+				auto itr = res.begin();
+				std::advance(itr, idx);
+				return itr;
+			};
+			switch(mtf({0, ActionN::_Num})) {
+				case ActionN::Acquire:
+				{
+					// 新たにリソースを確保
+					const auto val = rv();
+					const auto key = lubee::random::GenAlphabetString(mtf, 16);
+					res.emplace(key, Res{key, mgr.acquire(key, val), val});
+					break;
+				}
+				case ActionN::Release:
+					// リソースを解放
+					if(!res.empty()) {
+						res.erase(pickRandom());
+					}
+					break;
+				case ActionN::AcquireExist:
+					if(!res.empty()) {
+						// 既に存在するリソースを取得
+						// 同じポインタが返ってくればOK
+						const auto itr = pickRandom();
+						const auto r = mgr.acquire(itr->first, rv());
+						ASSERT_EQ(itr->second.res.get(), r.get());
+					}
+					break;
+				case ActionN::FindByName:
+					if(!res.empty()) {
+						// 既に存在するリソースを名前から検索
+						const auto itr = pickRandom();
+						ASSERT_TRUE(mgr.get(itr->first));
+					}
+					break;
+				case ActionN::FindByHandle:
+					if(!res.empty()) {
+						const auto itr = pickRandom();
+						// 既に存在するリソースのポインタから名前を検索
+						const auto key0 = mgr.getKey(itr->second.res);
+						const auto key1 = mgr.getKey(itr->second.res.get());
+						ASSERT_TRUE(key0);
+						ASSERT_TRUE(key1);
+						ASSERT_EQ(*key0, *key1);
+						const auto res0 = mgr.get(*key0);
+						ASSERT_EQ(itr->second.res, res0);
+					}
+					break;
+			}
+			// 各リソースの内部値が合致しているか
+			for(auto& r : res) {
+				ASSERT_EQ(r.second.val, r.second.res->getValue());
 			}
 			// コンストラクタ/デストラクタが呼ばれた回数を比較
 			// (ctor - dtor == リソースの残り数)
