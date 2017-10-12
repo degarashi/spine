7d16ea59c672cd90f737e7343821ea325dc93e39
degarashi <slice013@gmail.com>
2017-10-12 16:06:32 +0900

    noseq_list: namespaceに関するリファクタリング

--- noseq_list.hpp
+++ noseq_list.hpp
@@ -7,147 +7,144 @@
 #include "enum.hpp"
 
 namespace spi {
-	namespace {
-		namespace _noseq_list {
-			template <class T>
-			struct RemoveRef {
-				using T2 = typename std::remove_reference<T>::type;
-				using reference = T2&;
-				using pointer = T2*;
-			};
-			template <class T>
-			struct RemoveRef <T*> {
-				using reference = T*&;
-				using pointer = T**;
-			};
-			template <class Data, class Base>
-			class IteratorBase : public Base {
-				private:
-					using base_t = Base;
-				public:
-					using this_t = IteratorBase<Data, Base>;
-					using base_t::base_t;
-					using value_type = Data;
-					using pointer = typename RemoveRef<Data>::pointer;
-					using reference = typename RemoveRef<Data>::reference;
+	namespace _noseq_list {
+		template <class T>
+		struct RemoveRef {
+			using T2 = typename std::remove_reference<T>::type;
+			using reference = T2&;
+			using pointer = T2*;
+		};
+		template <class T>
+		struct RemoveRef <T*> {
+			using reference = T*&;
+			using pointer = T**;
+		};
+		template <class Data, class Base>
+		class IteratorBase : public Base {
+			private:
+				using base_t = Base;
+			public:
+				using this_t = IteratorBase<Data, Base>;
+				using base_t::base_t;
+				using value_type = Data;
+				using pointer = typename RemoveRef<Data>::pointer;
+				using reference = typename RemoveRef<Data>::reference;
 
-					IteratorBase() = default;
-					IteratorBase(const base_t& base):
-						base_t(base)
-					{}
-					pointer operator ->() const noexcept {
-						return &this->operator *();
-					}
-					reference operator *() const noexcept {
-						return (reference)base_t::operator*();
-					}
-					this_t& operator -- () noexcept {
-						base_t::operator --();
-						return *this;
-					}
-					this_t operator -- (const int) const noexcept {
-						const this_t ret = *this;
-						this->operator --();
-						return ret;
-					}
-					this_t& operator ++ () noexcept {
-						base_t::operator ++();
-						return *this;
-					}
-					this_t operator ++ (const int) const noexcept {
-						const this_t ret = *this;
-						this->operator ++();
-						return ret;
-					}
-					this_t operator + (const int n) const noexcept {
-						this_t ret = *this;
-						ret += n;
-						return ret;
-					}
-					this_t operator - (const int n) const noexcept {
-						this_t ret = *this;
-						ret -= n;
-						return ret;
-					}
-					using base_t::operator -;
+				IteratorBase() = default;
+				IteratorBase(const base_t& base):
+					base_t(base)
+				{}
+				pointer operator ->() const noexcept {
+					return &this->operator *();
+				}
+				reference operator *() const noexcept {
+					return (reference)base_t::operator*();
+				}
+				this_t& operator -- () noexcept {
+					base_t::operator --();
+					return *this;
+				}
+				this_t operator -- (const int) const noexcept {
+					const this_t ret = *this;
+					this->operator --();
+					return ret;
+				}
+				this_t& operator ++ () noexcept {
+					base_t::operator ++();
+					return *this;
+				}
+				this_t operator ++ (const int) const noexcept {
+					const this_t ret = *this;
+					this->operator ++();
+					return ret;
+				}
+				this_t operator + (const int n) const noexcept {
+					this_t ret = *this;
+					ret += n;
+					return ret;
+				}
+				this_t operator - (const int n) const noexcept {
+					this_t ret = *this;
+					ret -= n;
+					return ret;
+				}
+				using base_t::operator -;
 
-					this_t& operator += (const int n) noexcept {
-						base_t::operator += (n);
-						return *this;
-					}
-					this_t& operator -= (const int n) noexcept {
-						base_t::operator -= (n);
-						return *this;
-					}
-			};
-		}
+				this_t& operator += (const int n) noexcept {
+					base_t::operator += (n);
+					return *this;
+				}
+				this_t& operator -= (const int n) noexcept {
+					base_t::operator -= (n);
+					return *this;
+				}
+		};
+		//! ユーザーの要素を格納
+		template <class Value>
+		struct UData {
+			spi::Optional<Value>	value;
+			//! ユニークID。要素の配列内移動をする際に使用
+			id_t					uid;
+
+			UData() = default;
+			template <class T2>
+			UData(T2&& t, const id_t id):
+				value(std::forward<T2>(t)),
+				uid(id)
+			{}
+
+			bool operator == (const UData& ud) const noexcept {
+				return value == ud.value &&
+						uid == ud.uid;
+			}
+			template <class Ar, class V>
+			friend void serialize(Ar& ar, UData<V>&);
+		};
+		template <class Id>
+		struct IDS {
+			using id_t = Id;
+			DefineEnum(Type, (None)(Free)(Obj));
+			Type	type = Type::None;
+			id_t	value;
+
+			bool operator == (const IDS& ids) const noexcept {
+				return type == ids.type &&
+						value == ids.value;
+			}
+			static IDS AsFreeId(const id_t id) {
+				return IDS{Type::Free, id};
+			}
+			static IDS AsObjId(const id_t id) {
+				return IDS{Type::Obj, id};
+			}
+
+			template <class Ar, class Id2>
+			friend void serialize(Ar& ar, IDS<Id2>&);
+		};
+		template <class Value, class Id>
+		struct Entry {
+			using id_t = Id;
+			UData<Value>		udata;
+			IDS<Id>				ids;
+
+			Entry() = default;
+			template <class T2>
+			Entry(T2&& t, const id_t idx):
+				udata(std::forward<T2>(t),idx),
+				ids(IDS<Id>::AsObjId(idx))
+			{}
+			//! deserialize用
+			Entry(spi::Optional<Value>&& value, const id_t uid, const IDS<Id> ids):
+				udata(std::move(value), uid), ids(ids)
+			{}
+			bool operator == (const Entry& e) const {
+				return udata == e.udata &&
+						ids == e.ids;
+			}
+			template <class Ar, class V, class Id2>
+			friend void serialize(Ar& ar, Entry<V,Id2>&);
+		};
 	}
-
-	//! ユーザーの要素を格納
-	template <class Value>
-	struct UData {
-		spi::Optional<Value>	value;
-		//! ユニークID。要素の配列内移動をする際に使用
-		id_t					uid;
-
-		UData() = default;
-		template <class T2>
-		UData(T2&& t, const id_t id):
-			value(std::forward<T2>(t)),
-			uid(id)
-		{}
-
-		bool operator == (const UData& ud) const noexcept {
-			return value == ud.value &&
-					uid == ud.uid;
-		}
-		template <class Ar, class V>
-		friend void serialize(Ar& ar, UData<V>&);
-	};
-	template <class Id>
-	struct IDS {
-		using id_t = Id;
-		DefineEnum(Type, (None)(Free)(Obj));
-		Type	type = Type::None;
-		id_t	value;
-
-		bool operator == (const IDS& ids) const noexcept {
-			return type == ids.type &&
-					value == ids.value;
-		}
-		static IDS AsFreeId(const id_t id) {
-			return IDS{Type::Free, id};
-		}
-		static IDS AsObjId(const id_t id) {
-			return IDS{Type::Obj, id};
-		}
-
-		template <class Ar, class Id2>
-		friend void serialize(Ar& ar, IDS<Id2>&);
-	};
-	template <class Value, class Id>
-	struct Entry {
-		using id_t = Id;
-		UData<Value>		udata;
-		IDS<Id>				ids;
-
-		Entry() = default;
-		template <class T2>
-		Entry(T2&& t, const id_t idx):
-			udata(std::forward<T2>(t),idx),
-			ids(IDS<Id>::AsObjId(idx))
-		{}
-		//! deserialize用
-		Entry(spi::Optional<Value>&& value, const id_t uid, const IDS<Id> ids):
-			udata(std::move(value), uid), ids(ids)
-		{}
-		bool operator == (const Entry& e) const {
-			return udata == e.udata &&
-					ids == e.ids;
-		}
-		template <class Ar, class V, class Id2>
-		friend void serialize(Ar& ar, Entry<V,Id2>&);
-	};
 
 	//! 順序なしのID付きリスト
 	/*! 全走査を速く、要素の追加削除を速く(走査中はNG)、要素の順序はどうでもいい、あまり余計なメモリは食わないように・・というクラス */
@@ -157,12 +154,12 @@ namespace spi {
 		class IDType = uint_fast32_t
 	>
 	class noseq_list {
-		public:
+		private:
 			using this_t = noseq_list<T,Allocator,IDType>;
 			using id_t = IDType;
 			using value_t = T;
-			using entry_t = Entry<value_t, id_t>;
-			using ids_t = IDS<id_t>;
+			using entry_t = _noseq_list::Entry<value_t, id_t>;
+			using ids_t = _noseq_list::IDS<id_t>;
 
 			using Array = std::vector<entry_t, typename Allocator::template rebind<entry_t>::other>;
 			Array		_array;
@@ -325,5 +322,4 @@ namespace spi {
 						_array == lst._array;
 			}
 	};
-	namespace _noseq_list {}
 }
--- serialization/noseq_list.hpp
+++ serialization/noseq_list.hpp
@@ -4,26 +4,28 @@
 #include <cereal/types/vector.hpp>
 
 namespace spi {
-	template <class Ar, class Value>
-	void serialize(Ar& ar, UData<Value>& ns) {
-		ar(
-			cereal::make_nvp("value", ns.value),
-			cereal::make_nvp("uid", ns.uid)
-		);
-	}
-	template <class Ar, class Id>
-	void serialize(Ar& ar, IDS<Id>& id) {
-		ar(
-			cereal::make_nvp("type", id.type),
-			cereal::make_nvp("value", id.value)
-		);
-	}
-	template <class Ar, class Value, class Id>
-	void serialize(Ar& ar, Entry<Value,Id>& ent) {
-		ar(
-			cereal::make_nvp("udata", ent.udata),
-			cereal::make_nvp("ids", ent.ids)
-		);
+	namespace _noseq_list {
+		template <class Ar, class Value>
+		void serialize(Ar& ar, UData<Value>& ns) {
+			ar(
+				cereal::make_nvp("value", ns.value),
+				cereal::make_nvp("uid", ns.uid)
+			);
+		}
+		template <class Ar, class Id>
+		void serialize(Ar& ar, IDS<Id>& id) {
+			ar(
+				cereal::make_nvp("type", id.type),
+				cereal::make_nvp("value", id.value)
+			);
+		}
+		template <class Ar, class Value, class Id>
+		void serialize(Ar& ar, Entry<Value,Id>& ent) {
+			ar(
+				cereal::make_nvp("udata", ent.udata),
+				cereal::make_nvp("ids", ent.ids)
+			);
+		}
 	}
 	template <class Ar, class T, class Alc, class Id>
 	void serialize(Ar& ar, noseq_list<T,Alc,Id>& nl) {
